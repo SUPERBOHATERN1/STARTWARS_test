16af10176b35ce7f2b936ef4690b71efcf0b1430
SUPERBOHATERN1 <you@example.com>
2021-01-18 20:02:43 +0100

    unban dist4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import {App} from "/app/App.js";
+import {App} from "./app/App.js";
 
 const ONE_SECOND_MILLIS = 1000;
 const SW_API_BASE_URL = process.env.SW_API_BASE_URL || "https://swapi.dev/api";
